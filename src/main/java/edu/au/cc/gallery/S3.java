8afa3d1028b66a864818a834dad99a3ba4847a1f
Grant Phillips <grant.christopher.phillips@gmail.com>
2020-05-28 19:45:38 +0000

    Added deleteObject method

--- src/main/java/edu/au/cc/gallery/S3.java
+++ src/main/java/edu/au/cc/gallery/S3.java
@@ -5,44 +5,55 @@ import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.CreateBucketConfiguration;
 import software.amazon.awssdk.services.s3.model.CreateBucketRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
 import software.amazon.awssdk.core.sync.RequestBody;
 
 public class S3 {
 
-    private S3Client client;
-    private static final Region region = Region.US_EAST_2;
+	private S3Client client;
+	private static final Region region = Region.US_EAST_2;
 
-    public void connect() {
-	    client = S3Client.builder().region(region).build();
-    }
+	public void connect() {
+		client = S3Client.builder().region(region).build();
+	}
 
-    public void createBucket(String bucketName) {
-	    CreateBucketRequest createBucketRequest = CreateBucketRequest
-            .builder()
-            .bucket(bucketName)
-	        .createBucketConfiguration(CreateBucketConfiguration.builder()
-        	    .locationConstraint(region.id())
-                .build())
-	        .build();   
-	    client.createBucket(createBucketRequest);
-    }
+	public void createBucket(String bucketName) {
+		CreateBucketRequest createBucketRequest = CreateBucketRequest
+			.builder()
+			.bucket(bucketName)
+			.createBucketConfiguration(CreateBucketConfiguration.builder()
+					.locationConstraint(region.id())
+					.build())
+			.build();   
+		client.createBucket(createBucketRequest);
+	}
 
-    public void putObject(String bucketName, String key, String value) {
-	    PutObjectRequest por = PutObjectRequest.builder()
-	        .bucket(bucketName)
-	        .key(key)
-	        .build();
+	public void putObject(String bucketName, String key, String value) {
+		PutObjectRequest por = PutObjectRequest.builder()
+			.bucket(bucketName)
+			.key(key)
+			.build();
 
-	    client.putObject(por, RequestBody.fromString(value));
-	
-    }
+		client.putObject(por, RequestBody.fromString(value));
 
-    public static void demo() {
-	    String bucketName = "edu.au.gcp0015.image-gallery";
-    	S3 s3 = new S3();
-	    s3.connect();
-    //	s3.createBucket(bucketName);
-	    s3.putObject(bucketName, "banana", "yellow");
-    }
+	}
+
+	public void deleteObject(String bucketName, String key) {
+		DeleteObjectRequest dor = DeleteObjectRequest.builder()
+			.bucket(bucketName)
+			.key(key)
+			.build();
+		client.deleteObject(dor);
+
+	}
+
+	public static void demo() {
+		String bucketName = "edu.au.gcp0015.image-gallery";
+		S3 s3 = new S3();
+		s3.connect();
+		//	s3.createBucket(bucketName);
+		// s3.putObject(bucketName, "banana", "yellow");
+		s3.deleteObject(bucketName, "banana");
+	}
 
 }
