b5ebfb03c1856b6246630d97c02694876a64dd83
Grant Phillips <grant.christopher.phillips@gmail.com>
2020-05-28 19:01:45 +0000

    Added S3 utility class

--- src/main/java/edu/au/cc/gallery/App.java
+++ src/main/java/edu/au/cc/gallery/App.java
@@ -4,11 +4,7 @@
 package edu.au.cc.gallery;
 
 public class App {
-    public String getGreeting() {
-        return "Hello Grant.";
-    }
-
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+    	S3.demo();
     }
 }
