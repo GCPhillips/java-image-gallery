0dfda614a7001ac98301d56a802d54badd3d7045
Grant Phillips <grant.christopher.phillips@gmail.com>
2020-05-28 19:19:17 +0000

    Fixed tab spacing in vim

--- src/main/java/edu/au/cc/gallery/S3.java
+++ src/main/java/edu/au/cc/gallery/S3.java
@@ -13,36 +13,36 @@ public class S3 {
     private static final Region region = Region.US_EAST_2;
 
     public void connect() {
-	client = S3Client.builder().region(region).build();
+	    client = S3Client.builder().region(region).build();
     }
 
     public void createBucket(String bucketName) {
-	CreateBucketRequest createBucketRequest = CreateBucketRequest
+	    CreateBucketRequest createBucketRequest = CreateBucketRequest
             .builder()
             .bucket(bucketName)
-	    .createBucketConfiguration(CreateBucketConfiguration.builder()
+	        .createBucketConfiguration(CreateBucketConfiguration.builder()
         	    .locationConstraint(region.id())
-               	    .build())
-	    .build();   
-	client.createBucket(createBucketRequest);
+                .build())
+	        .build();   
+	    client.createBucket(createBucketRequest);
     }
 
     public void putObject(String bucketName, String key, String value) {
-	PutObjectRequest por = PutObjectRequest.builder()
-	    .bucket(bucketName)
-	    .key(key)
-	    .build();
+	    PutObjectRequest por = PutObjectRequest.builder()
+	        .bucket(bucketName)
+	        .key(key)
+	        .build();
 
-	client.putObject(por, RequestBody.fromString(value));
+	    client.putObject(por, RequestBody.fromString(value));
 	
     }
 
     public static void demo() {
-	String bucketName = "edu.au.gcp0015.image-gallery";
-	S3 s3 = new S3();
-	s3.connect();
-//	s3.createBucket(bucketName);
-	s3.putObject(bucketName, "banana", "yellow");
+	    String bucketName = "edu.au.gcp0015.image-gallery";
+    	S3 s3 = new S3();
+	    s3.connect();
+    //	s3.createBucket(bucketName);
+	    s3.putObject(bucketName, "banana", "yellow");
     }
 
 }
