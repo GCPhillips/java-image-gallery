fddb34f3117329c21ca8db180f4f4e2d9b00df69
Grant Phillips <grant.christopher.phillips@gmail.com>
2020-06-20 01:15:24 +0000

    fixed missing quotation in prod script

--- ec2-scripts/ec2-prod-1.0.sh
+++ ec2-scripts/ec2-prod-1.0.sh
@@ -4,7 +4,7 @@ yum -y update
 yum install -y java-11-openjdk-devel git
 amazon-linux-extras install -y java-openjdk11
 amazon-linux-extras install -y nginx1
-su ec2-user -l -c 'curl -s "https://get.sdkman.io | bash && source .bashrc && sdk install gradle'
+su ec2-user -l -c 'curl -s "https://get.sdkman.io" | bash && source .bashrc && sdk install gradle'
 
 cd /home/ec2-user
 git clone https://github.com/gcphillips/java-image-gallery.git
